831ec95d61f95cfc0ca7d10debdb5778d65ef5a2
karan-mittal1 <karan.mittal@jellyfishtechnologies.com>
2018-06-21 11:18:10 +0530

    20/june/2018 local storage login and register

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import sunSet from './images/sunSet.jpeg'
+import Login from "./LoginPage";
+import Register from "./RegisterPage";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
-          <h1 className="App-title">Welcome to friend's book</h1>
+            <img src={sunSet}
+                 alt="sun set"
+                 width="1800" height="120" 
+            />
+          <h1 className="App-title">Welcome to Jellyfish Technologies</h1>
         </header>
+
+
+          <Register/>
+          <Login/>
+
 
       </div>
     );
